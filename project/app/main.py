eeeb35f385b6fec54811af68ad16c5951ddc336b
gian grobert <gianmamani93@gmail.com>
2022-10-31 16:13:08 -0500

    V07

--- project/app/main.py
+++ project/app/main.py
@@ -1,25 +1,27 @@
 # project/app/main.py
+ 
+ 
 import os
-
-from fastapi import FastAPI, Depends
+ 
+from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from app.config import get_settings, Settings
-
-app = FastAPI()
-
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
-
-
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+from app.api import ping
+ 
+ 
+def create_application() -> FastAPI:
+    application = FastAPI()
+ 
+    register_tortoise(
+   	 application,
+   	 db_url=os.environ.get("DATABASE_URL"),
+   	 modules={"models": ["app.models.tortoise"]},
+   	 generate_schemas=False,
+   	 add_exception_handlers=True,
+    )
+ 
+    application.include_router(ping.router)
+ 
+    return application
+ 
+ 
+app = create_application()
--- project/test/conftest.py
+++ project/test/conftest.py
@@ -1,26 +1,27 @@
 # project/tests/conftest.py
-
-
+ 
+ 
 import os
-
+ 
 import pytest
 from starlette.testclient import TestClient
-
-from app import main
+ 
+from app.main import create_application  # updated
 from app.config import get_settings, Settings
-
-
+ 
+ 
 def get_settings_override():
     return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
-
-
+ 
+ 
 @pytest.fixture(scope="module")
 def test_app():
     # set up
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
-
-        # testing
-        yield test_client
-
+    app = create_application()  # new
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:  # updated
+ 
+   	 # testing
+   	 yield test_client
+ 
     # tear down

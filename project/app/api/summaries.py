17885cf7825cecf3496ad33a05c24f4a5db4d0e5
gian grobert <gianmamani93@gmail.com>
2022-11-28 15:03:18 -0500

    test update summaries

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -6,8 +6,11 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import (SummaryPayloadSchema, SummaryResponseSchema,
-                                 SummaryUpdatePayloadSchema)
+from app.models.pydantic import (
+    SummaryPayloadSchema,
+    SummaryResponseSchema,
+    SummaryUpdatePayloadSchema,
+)
 from app.models.tortoise import SummarySchema
 
 router = APIRouter()

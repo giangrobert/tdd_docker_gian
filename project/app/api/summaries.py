e9e3fae9e14e4e5d4e921300aeeb25b3f1177648
gian grobert <gianmamani93@gmail.com>
2022-11-28 15:11:49 -0500

    update summaries

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -3,15 +3,16 @@
 
 from typing import List
 
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, HTTPException
 
 from app.api import crud
-from app.models.pydantic import (
+from app.models.tortoise import SummarySchema
+
+from app.models.pydantic import (  # isort:skip
     SummaryPayloadSchema,
     SummaryResponseSchema,
     SummaryUpdatePayloadSchema,
 )
-from app.models.tortoise import SummarySchema
 
 router = APIRouter()
 
@@ -51,9 +52,7 @@ async def delete_summary(id: int) -> SummaryResponseSchema:
 
 
 @router.put("/{id}/", response_model=SummarySchema)
-async def update_summary(
-    payload: SummaryUpdatePayloadSchema, id: int = Path(..., gt=0)
-) -> SummarySchema:
+async def update_summary(id: int, payload: SummaryUpdatePayloadSchema) -> SummarySchema:
     summary = await crud.put(id, payload)
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
